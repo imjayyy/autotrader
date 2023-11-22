70fe33a7e23070754f29c6787cd6017599d6c546
root <root@Mujtaba-EXP-JBS>
2023-11-23 01:54:45 +0500

    c3

--- autotrader_web/car_details/views.py
+++ autotrader_web/car_details/views.py
@@ -122,7 +122,7 @@ class CarDetailsView(TemplateView):
             #         location_state = "N/A"
             #         country = "N/A"
             #         location_port = "N/A"
-            city = Cities.objects.filter(Name__icontains=lot_data_model.locationName)[0]
+            city = Cities.objects.get_or_create(Name__icontains=lot_data_model.locationName)[0]
             location_city = city.Name
             location_state = city.State
             location_port = city.Port
